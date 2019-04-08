169f3f3b2a58515e329a1439952c10ea623d037f
Massimo Cappellano <Massimo.Cappe@gmail.com>
2019-04-08 15:18:16 +0200

    Initial import

---
+++ src/EchoServer.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoServer {
+	private int port;
+	private ServerSocket serverSocket;
+
+	public EchoServer(int port) {
+		this.port = port;
+	}
+
+	public void startServer() throws IOException {
+		// apro una porta TCP
+		serverSocket = new ServerSocket(port);
+		System.out.println("Server	socket	ready on port:" + port);
+		// resto in attesa di una connessione
+		Socket socket = serverSocket.accept();
+		System.out.println("Received client	connection");
+		// apro gli stream di input e output per leggere
+		// e scrivere nella connessione appena ricevuta
+		Scanner in = new Scanner(socket.getInputStream());
+		PrintWriter out = new PrintWriter(socket.getOutputStream());
+		// leggo e scrivo nella connessione finche'non
+		// ricevo "quit"
+		while (true) {
+			String line = in.nextLine();
+			if (line.equals("quit")) {
+				break;
+			} else {
+				out.println("Received:	" + line);
+				out.flush();
+			}
+		}
+		// chiudo gli stream e il socket
+		System.out.println("Closing	sockets");
+		in.close();
+		out.close();
+		socket.close();
+		serverSocket.close();
+	}
+
+	public static void main(String[] args) {
+		EchoServer server = new EchoServer(1337);
+		try {
+			server.startServer();
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+}
---
+++ src/MultiEchoServer.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MultiEchoServer {
+	private int port;
+
+	public MultiEchoServer(int port) {
+		this.port = port;
+	}
+
+	public void startServer() {
+		ExecutorService executor = Executors.newCachedThreadPool();
+		ServerSocket serverSocket;
+		try {
+			serverSocket = new ServerSocket(port);
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+			// porta non disponibile
+			return;
+		}
+		System.out.println("Server ready");
+		while (true) {
+			try {
+
+				Socket socket = serverSocket.accept();
+				executor.submit(new EchoServerClientHandler(socket));
+			} catch (IOException e) {
+				break;
+				// entrerei qui se serverSocket venisse chiuso
+			}
+		}
+		executor.shutdown();
+	}
+
+	public static void main(String[] args) {
+		MultiEchoServer echoServer = new MultiEchoServer(1337);
+		echoServer.startServer();
+	}
+}
